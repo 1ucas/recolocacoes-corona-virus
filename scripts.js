762ff35cb4ac6c6967349c037d79a7abc33c49ff
Lucas Maciel <lucasramos.m@gmail.com>
2020-04-07 00:18:30 -0300

    Setup Html Inicial

---
+++ scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    // Sim pessoal, a programação é em Português porque o objetivo neste momento é sermos o mais inclusivos possível.
+
+    carregarTodosDados()
+
+});
+
+function carregarTodosDados() {
+    
+}
