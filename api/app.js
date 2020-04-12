4f6744ed1519b98d24d8999d9db63164a02979e1
Lucas Maciel <lucasramos.m@gmail.com>
2020-04-12 19:50:03 -0300

    rotas funcionando

--- api/app.js
+++ api/app.js
@@ -3,81 +3,42 @@ const fs = require('fs');
 var app = express();
 const caminhoRaiz = "../dados/";
 
+const listarFuncionarios = function(cidade,empresa){
+    var informacoesEmpresa = fs.readFileSync(`${caminhoRaiz}//${cidade}//${empresa}//dados.json`);
 
-const obtemConteudoArquivo = function(conteudo){
-    const arquivo = JSON.parse(conteudo);
-    return arquivo;
-}
-const leArquivo = function(nomeCidade, nomeEmpresa, nomeArquivo){
-    fs.readFileSync(`${caminhoRaiz}${nomeCidade}/${nomeEmpresa}/${nomeArquivo}`, function read(err, conteudoArquivo) {
-        if (err) {
-            throw err;
-        }
-        return obtemConteudoArquivo(conteudoArquivo)
-    });
+    return JSON.parse(informacoesEmpresa).desligamentos;
 }
 
+const listarEmpresas = function(cidade){
+    var list = fs.readdirSync(`${caminhoRaiz}//${cidade}`);
 
-const leDiretorioEmpresa = function(nomeCidade, nomeEmpresa){
-    let objetoEmpresa = {
-        nomeEmpresa : []
-    }
-    fs.readdirSync(`${caminhoRaiz}${nomeCidade}/${nomeEmpresa}`, function (err, arquivos) {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        for (const arquivo of arquivos) {
-            let conteudoArquivo = leArquivo(nomeCidade, nomeEmpresa, arquivo)
-            objetoEmpresa.nomeEmpresa.push(conteudoArquivo);
-        }
-    });
-    return objetoEmpresa;
+    return list;
 }
 
-const leDiretorioCidade = function(nomeCidade){
-    let objetoCidade = {
-        nomeCidade : []
-    }
-    fs.readdirSync(`${caminhoRaiz}${nomeCidade}`, function (err, empresas) {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        for (const empresa of empresas) {
-            let conteudoEmpresa = leDiretorioEmpresa(nomeCidade ,empresa)
-            objetoCidade.nomeCidade.push(conteudoEmpresa);
-        }
-    });
+const listarCidades = function(){
+    var list = fs.readdirSync(`${caminhoRaiz}`);
 
-    return objetoCidade;
+    return list;
 }
 
-const leDiretorioDados = function(){
-    let objetoCompleto = {
-        cidades : []
-    };
-    
-    fs.readdirSync(`${caminhoRaiz}`, function (err, cidades) {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        for (const cidade of cidades) {
-            let conteudoCidade = leDiretorioCidade(cidade)
-            objetoCompleto.cidades.push(conteudoCidade);
-        }
-    });
+app.get('/cidades/:cidade/:empresa', function(req, res) {
+    const objetoARetornar = listarFuncionarios(req.params.cidade, req.params.empresa);
+    console.log(objetoARetornar)
+    res.send(objetoARetornar);
+});
 
-    return objetoCompleto;
-}
+app.get('/cidades/:cidade', function(req, res) {
+    const objetoARetornar = listarEmpresas(req.params.cidade);
+    console.log(objetoARetornar)
+    res.send(objetoARetornar);
+});
 
 app.get('/cidades', function(req, res) {
-    const objetoARetornar = leDiretorioDados();
+    const objetoARetornar = listarCidades();
     console.log(objetoARetornar)
     res.send(objetoARetornar);
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Local server listening on port 3000!');
 });
